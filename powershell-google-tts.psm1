7ef54b3a0ff093758ae6691ba93b7c6ba9bc96b0
jmbeach1 <jaredbeachdesign@gmail.com>
2020-08-21 10:35:37 -0500

    adds ability to conactenate multiple requests into an out file

--- powershell-google-tts.psm1
+++ powershell-google-tts.psm1
@@ -9,25 +9,56 @@ function Start-GoogleTTS ($text, $speed, $outFile) {
         $audioConfig | Add-Member -NotePropertyName 'speakingRate' -NotePropertyValue $speed
     }
 
+    $i = 0;
+    $j = 0;
+    $parts = $text.Split(' ');
+    $outFiles = [System.Collections.Generic.List[string]]::new();
+    $useTemp = $null -eq $outFile;
     $input = [psobject]::new();
-    $input | Add-Member -NotePropertyName 'text' -NotePropertyValue $text;
+    $input | Add-Member -NotePropertyName 'text' -NotePropertyValue $null;
     $voice = [psobject]::new();
     $voice | Add-Member -NotePropertyName 'languageCode' -NotePropertyValue 'en-US';
     $voice | Add-Member -NotePropertyName 'name' -NotePropertyValue 'en-US-Standard-C';
     $data | Add-Member -NotePropertyName 'audioConfig' -NotePropertyValue $audioConfig;
     $data | Add-Member -NotePropertyName 'input' -NotePropertyValue $input;
     $data | Add-Member -NotePropertyName 'voice' -NotePropertyValue $voice;
-    $response = Invoke-WebRequest -Uri ($baseUrl + 'text:synthesize?key=' + $env:GOOGLE_API_KEY_TTS) -ContentType 'application/json' -Body (ConvertTo-Json $data) -Method POST;
-    $audioData = $response.Content | ConvertFrom-Json;
-    $base64 = [System.Convert]::FromBase64String($audioData.audioContent);
-    $tmpFile = $env:TEMP + "\" + [System.DateTime]::Now.ToSTring('yyyy-MM-dd-hh-mm-ss-ff') + '.mp3';
-    if ($null -eq $outFile) {
-        $outFile = $tmpFile;
+    while ($i -lt $text.Length / 5000) {
+        $chunk = '';
+        while ($j -lt $parts.Length -and ($chunk.Length + $parts[$j].Length + 1) -lt 5000) {
+            $chunk += $parts[$j] + ' ';
+            $j++;
+        }
+
+        $input.text = $chunk;
+        $response = Invoke-WebRequest -Uri ($baseUrl + 'text:synthesize?key=' + $env:GOOGLE_API_KEY_TTS) -ContentType 'application/json' -Body (ConvertTo-Json $data) -Method POST;
+        $audioData = $response.Content | ConvertFrom-Json;
+        $base64 = [System.Convert]::FromBase64String($audioData.audioContent);
+        $tmpFile = $env:TEMP + "\" + [System.DateTime]::Now.ToSTring('yyyy-MM-dd-hh-mm-ss-ff') + ".mp3";
+        $writeFile = "$outFile.$i";
+        if ($useTemp) {
+            $writeFile = $tmpFile;
+        }
+
+        [System.IO.File]::WriteAllBytes($writeFile, $base64);
+        if ($useTemp) {
+            ffplay.exe -nodisp -autoexit -hide_banner -loglevel panic $writeFile > $null;
+            Remove-Item $writeFile -Force;
+        }
+        else {
+            # add it to file for concatenation
+            $outFiles.Add($writeFile);
+            "file '$writeFile'" | Out-File 'concat.txt' -Append -Encoding ascii;
+        }
+
+        $i++;
     }
 
-    [System.IO.File]::WriteAllBytes($tmpFile, $base64);
-    ffplay.exe -nodisp -autoexit -hide_banner -loglevel panic $outFile > $null;
-    if ($null -eq $outFile) {
-        Remove-Item $tmpFile -Force;
+    if (-not $useTemp) {
+        ffmpeg -f concat -safe 0 -i concat.txt -c copy $outFile
+        $outFiles | ForEach-Object {
+            Remove-Item $_ -Force
+        }
+
+        Remove-Item concat.txt -Force
     }
 }
