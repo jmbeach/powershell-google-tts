a629f504383425389ff6e91b73b740dc954bc8f4
jmbeach1 <jaredbeachdesign@gmail.com>
2020-08-21 09:23:41 -0500

    adds out file option

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This powershell module synthesizes speech from text in Powershell using [Google's Text-to-Speech API](https://cloud.google.com/text-to-speech/)
 
-To use this module, install it from the [Powershell Gallery](https://www.powershellgallery.com/packages/powershell-google-tts);
+To use this module, install it from the [Powershell Gallery](https://www.powershellgallery.com/packages/powershell-google-tts)
 
 ```
 Install-Module -Name powershell-google-tts
@@ -11,3 +11,11 @@ Install-Module -Name powershell-google-tts
 Add an environment variable `GOOGLE_API_KEY_TTS` and set the value to a valid API key.
 
 Then, in Powershell, run `Start-GoogleTTS 'your text here'`
+
+# Examples
+
+## Speak Text From File
+
+```
+Start-GoogleTTS $(cat .\example.txt).ToString()
+```
--- powershell-google-tts.psm1
+++ powershell-google-tts.psm1
@@ -1,7 +1,7 @@
 $baseUrl = 'https://texttospeech.googleapis.com/v1/';
 
 # requires ffplay (can be installed from ffmpeg)
-function Start-GoogleTTS ($text, $speed) {
+function Start-GoogleTTS ($text, $speed, $outFile) {
     $data = [psobject]::new()
     $audioConfig = [psobject]::new();
     $audioConfig | Add-Member -NotePropertyName 'audioEncoding' -NotePropertyValue 'MP3';
@@ -17,11 +17,17 @@ function Start-GoogleTTS ($text, $speed) {
     $data | Add-Member -NotePropertyName 'audioConfig' -NotePropertyValue $audioConfig;
     $data | Add-Member -NotePropertyName 'input' -NotePropertyValue $input;
     $data | Add-Member -NotePropertyName 'voice' -NotePropertyValue $voice;
-    $response = Invoke-WebRequest -Uri ($baseUrl + 'text:synthesize?key=' + $env:GOOGLE_API_KEY_TTS) -ContentType 'application/json' -Body (ConvertTo-Json $data) -Method POST
+    $response = Invoke-WebRequest -Uri ($baseUrl + 'text:synthesize?key=' + $env:GOOGLE_API_KEY_TTS) -ContentType 'application/json' -Body (ConvertTo-Json $data) -Method POST;
     $audioData = $response.Content | ConvertFrom-Json;
     $base64 = [System.Convert]::FromBase64String($audioData.audioContent);
     $tmpFile = $env:TEMP + "\" + [System.DateTime]::Now.ToSTring('yyyy-MM-dd-hh-mm-ss-ff') + '.mp3';
+    if ($null -eq $outFile) {
+        $outFile = $tmpFile;
+    }
+
     [System.IO.File]::WriteAllBytes($tmpFile, $base64);
-    ffplay.exe -nodisp -autoexit -hide_banner -loglevel panic $tmpFile > $null
-    Remove-Item $tmpFile -Force
+    ffplay.exe -nodisp -autoexit -hide_banner -loglevel panic $outFile > $null;
+    if ($null -eq $outFile) {
+        Remove-Item $tmpFile -Force;
+    }
 }
